9f259ee4421668c1b2fa3d359ed27dfdfa185fd4
Roman-24 <xbitarovsky@stuba.sk>
2021-06-22 22:31:21 +0200

    Spojenie usera a profilu
    Prepojenie, že vždy keď vytvoríme alebo vymažeme usera tak rovnaká akcia sa stane aj s profilom, pretože profil je automatický priradený k userovi

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -1,2 +1,16 @@
 # bude sluzit nato aby pri vytvoreni usera sa vzdy vytvoril aj profil pre daneho usera
 
+from .models import Profile
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def post_save_create_profile(sender, instance, created, **kwargs):
+
+    print(sender)
+    print(instance)
+    print(created)
+    
+    if created:
+        Profile.objects.create(user=instance)
