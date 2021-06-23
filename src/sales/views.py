499b238539d31b3dcaa65155f99301197e63aae5
Roman-24 <xbitarovsky@stuba.sk>
2021-06-23 16:20:29 +0200

    dataframe pre positions

--- src/sales/views.py
+++ src/sales/views.py
@@ -8,6 +8,7 @@ from .forms import SalesSearchForm
 
 def home_view(request):
     sales_df = None
+    positions_df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -17,18 +18,32 @@ def home_view(request):
 
         print(date_from, date_to, chart_type)
 
-        qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
 
-        if len(qs) > 0:
-            sales_df = pd.DataFrame(qs.values())
+        if len(sale_qs) > 0:
+            sales_df = pd.DataFrame(sale_qs.values())
+            positions_data = []
 
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price,
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data)
             sales_df = sales_df.to_html()
+            positions_df = positions_df.to_html()
         else:
             print("No data..")
 
     context = {
         'form': form,
         'sales_df': sales_df,
+        'positions_df': positions_df,
     }
     return render(request, 'sales/home.html', context)
 
