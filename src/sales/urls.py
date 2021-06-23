851f748e1ee94803c67eecd0f66e8212f1340f5d
Roman-24 <xbitarovsky@stuba.sk>
2021-06-23 10:58:49 +0200

    sales detail html

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     home_view,
     SaleListView,
+    SaleDeatailView
 )
 
 app_name = 'sales'
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('list/', SaleListView.as_view(), name='list'),
+    path('sales/', SaleListView.as_view(), name='list'),
+    path('sales/<pk>/', SaleDeatailView.as_view(), name='detail'),
 ]
